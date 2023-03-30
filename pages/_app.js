a3a24f07f4aa37d937d099655754230769ef2f1a
Frank Basham <frankabasham@gmail.com>
2023-03-30 19:30:59 -0400

    add side section

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { useSession } from "next-auth/react";
+
+function Auth({ children }) {
+  const { status } = useSession({ required: true });
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  return children;
+}
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      {Component.auth ? (
+        <Auth>
+          <Component {...pageProps} />
+        </Auth>
+      ) : (
+        <Component {...pageProps} />
+      )}
     </SessionProvider>
   );
 }
--- pages/signin.js
+++ pages/signin.js
@@ -1,11 +1,19 @@
 import { getServerSession } from "next-auth";
-import { getProviders, signIn, getCsrfToken } from "next-auth/react";
+import {
+  getProviders,
+  signIn,
+  getCsrfToken,
+  useSession,
+} from "next-auth/react";
 import { authOptions } from "./api/auth/[...nextauth]";
 
 export default function signin({ providers, csrfToken }) {
+  let session = useSession();
+  console.log(session);
+
   return (
     <main className="flex flex-col items-center justify-center h-[100vh]">
-      <div className="w-1/2 bg-blue-100 p-10">
+      <div className="w-[25%] bg-blue-100 p-10 space-y-10">
         <h1 className="text-2xl font-bold">Sign into Mood </h1>
         <div className="mt-5 space-y-5">
           {Object.values(providers).map(
@@ -21,17 +29,34 @@ export default function signin({ providers, csrfToken }) {
               )
           )}
         </div>
-        <form method="post" action="/api/auth/callback/credentials">
+        <form
+          method="post"
+          action="/api/auth/callback/credentials"
+          className="flex flex-col gap-4"
+        >
           <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-          <label>
+          <label className="flex flex-col font-semibold">
             Username
-            <input name="username" type="text" />
+            <input
+              name="username"
+              type="text"
+              className="py-1 px-2 font-normal"
+            />
           </label>
-          <label>
+          <label className="flex flex-col font-semibold">
             Password
-            <input name="password" type="password" />
+            <input
+              name="password"
+              type="password"
+              className="py-1 px-2 font-normal"
+            />
           </label>
-          <button type="submit">Sign in</button>
+          <button
+            type="submit"
+            className="w-1/2 bg-rose-700 text-white hover:bg-rose-900 rounded block mx-auto mt-2"
+          >
+            Sign in
+          </button>
         </form>
       </div>
     </main>
